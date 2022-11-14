7934949a83f2cce8be4a50aa5784631ccc4f0838
Andy Huynh <andy4thehuynh@gmail.com>
2022-11-13 21:37:49 -0700

    ALH: Add Neovim nvim-surround plugin

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -26,6 +26,13 @@ return require("packer").startup(function()
   use 'vim-ruby/vim-ruby'
 
 
+  use{
+    "kylechui/nvim-surround",
+    tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+    config = function() require("nvim-surround").setup({}) end
+  }
+
+
   use {
     'numToStr/Comment.nvim',
     config = function() require('Comment').setup() end
