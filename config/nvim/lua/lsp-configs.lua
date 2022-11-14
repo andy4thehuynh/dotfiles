d4b7768ea9c0b032627aa44e703c38bf7bde580c
Andy Huynh <andy4thehuynh@gmail.com>
2022-11-13 17:50:56 -0700

    ALH: Adds LSP support for Lua

--- config/nvim/lua/lsp-configs.lua
+++ config/nvim/lua/lsp-configs.lua
@@ -4,33 +4,45 @@
 
 local config = require("lspconfig")
 local lsp_defaults = config.util.default_config
--- local servers = { "solargraph", "gopls", "tsserver" }
 
 require("mason").setup()
-require("mason-lspconfig").setup({
-  ensure_installed = { "solargraph" }
-})
-
+require("mason-lspconfig").setup()
 lsp_defaults.capabilities = vim.tbl_deep_extend(
   "force",
   lsp_defaults.capabilities,
   require("cmp_nvim_lsp").default_capabilities()
 )
 
+
 ---
 -- LSP Servers
 ---
 
 config.solargraph.setup({})
+config.sumneko_lua.setup({
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+})
 
 
--- for _, server in pairs(servers) do
---   config[server].setup {
---     -- capabilities = defaultCapabilities,
---   }
--- end
---
---
 -- -- longer updatetime (default is 4000 ms = 4 s) leads to delays
 -- vim.o.updatetime = 250
 --
