c5527521b56dd72b8c5c78bfd561af1307aa4764
Andy Huynh <andy4thehuynh@gmail.com>
2022-11-15 11:17:31 -0700

    AH: Updates vim options with comment descriptions

--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -1,43 +1,41 @@
 local set = vim.api.nvim_set_option
 
+vim.o.termguicolors = true -- expands usable colors, if terminal supports it
+vim.cmd[[colorscheme dracula]] -- sets Neovim theme color
 
--- Basic Options
-set("swapfile", false)
-set("expandtab", true)
-set("relativenumber", true)
-set("number", true)
-set("ignorecase", true)
-set("compatible", false)
-set("showcmd", true)
-set("showmode", true)
-set("hidden", true)
-set("wildmenu", true)
-set("wildmode", "list:longest")
-set("smartcase", true)
-set("incsearch", true)
-set("hlsearch", true)
-set("wrap", true)
-set("scrolloff", 10)
-set("ruler", true)
-set("title", true)
-set("backup", false)
-set("writebackup", false)
-set("expandtab", true)
-set("laststatus", 2)
-set("shiftwidth", 2)
-set("tabstop", 2)
-set("shell", "/bin/bash")
-
-
--- nvim-cmp documentation says should set completopt with these values
+-- nvim-cmp documentation suggests setting completopt with these values
 vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
 
 
-vim.cmd [[
-  set clipboard+=unnamedplus
-]]
+--
+-- Basic Options
+--
+set("swapfile", false) -- disable creation of Vim .swp files
+set("clipboard", "unnamedplus") -- enables clipboard between nvim and other programs
+set("expandtab", true) -- tabs into spaces
+set("shiftwidth", 2) -- number of spaces when indenting the text
+set("tabstop", 2) -- number of spaces for tabs
+set("relativenumber", true) -- shows lines numbers heliocentric from current one
+set("number", true) -- shows the line numbers
+set("smartcase", true) -- case-insensitive if you only use lowercase letters;
+set("ignorecase", true) -- all searches will be case insensitive
+set("showcmd", true) -- shows partial commands in the last line of the screen
+set("showmode", true) -- insert, replace or visual mode put on last line as a message
+set("hidden", true) -- hide unused buffers
+set("wildmenu", true) -- shows a more advanced menu for autocomplete suggestions
+set("wildmode", "list:longest") -- displays completion mode
+set("hlsearch", true) -- highlights search patterns
+set("wrap", true) -- ensures text visibility stays within the window
+set("scrolloff", 10) --  minimal number of screen lines to keep above and below the cursor
+set("ruler", true) -- shows cursor position in a file
+set("title", true) -- shows the title of the file
+set("backup", false) -- disables creation of backup files
+set("writebackup", false) -- disable backup writes of a current file
+set("laststatus", 2) -- displays the status line, always
+set("shell", "/bin/zsh") -- sets shell for Vim
 
-
--- Set colorscheme
-vim.o.termguicolors = true
-vim.cmd[[colorscheme dracula]]
+-- automatic syntax detection support for open files
+vim.cmd([[
+  filetype plugin indent on
+  syntax on
+]])
