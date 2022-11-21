eeaa4d01b40a818921daf3d099c20b3f052fd069
Andy Huynh <andy4thehuynh@gmail.com>
2022-11-21 11:02:27 -0700

    alh: Tell Telescope to ignore node_modules and vendor dir

--- config/nvim/lua/andy4thehuynh/telescope/init.lua
+++ config/nvim/lua/andy4thehuynh/telescope/init.lua
@@ -1,13 +1,14 @@
 require("telescope").setup{
   defaults = {
+    file_ignore_patterns =
+      {
+        "node_modules",
+        "vendor",
+      },
   },
   pickers = {
-    find_files = {
-      theme = "dropdown",
-    },
-    live_grep = {
-      theme = "dropdown",
-    }
+    find_files = { theme = "dropdown" },
+    live_grep = { theme = "dropdown" }
   },
   extensions = {
     media_files = {
