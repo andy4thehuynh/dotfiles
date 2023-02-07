d22b6805a30803594a8bc3e3677f0d8ea213e2b4
Andy Huynh <andy4thehuynh@gmail.com>
2023-02-07 12:20:52 -0700

    ALH: Add mappings for vim from old config

--- config/nvim/custom/mappings.lua
+++ config/nvim/custom/mappings.lua
@@ -4,8 +4,11 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
-    ["<leader>n"] = { "<cmd> NvimTreeToggle <CR>", "   toggle nvimtree" },
+    ["<leader>n"] = { "<cmd> NvimTreeToggle <CR>", "toggle nvimtree" },
     ["<C-G>"] = { ":LazyGit<CR>", "open LazyGit", opts = { noremap = true } },
+    ["<leader>-"] = { ":split<cr>", "splits vim pane horizontally" },
+    ["<leader><leader>"] = { ":vsplit<cr>", "splits vim pane vertically" },
+    ["<leader>pi"] = { ":PackerSync<cr>", "installs plugins via Packer" },
   },
 }
 
