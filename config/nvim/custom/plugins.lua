9f96edae94002acce387356d5ee99758dafc0375
Andy Huynh <andy4thehuynh@gmail.com>
2023-04-08 21:44:15 -0600

    ALH: Adds support for ruby and rails development

--- config/nvim/custom/plugins.lua
+++ config/nvim/custom/plugins.lua
@@ -21,6 +21,29 @@ local plugins = {
   },
 
   {
+    "tpope/vim-rails",
+    ft = "ruby",
+    config = function()
+      vim.g.rails_detect_filetype = 1
+    end
+  },
+
+  {
+    "vim-ruby/vim-ruby",
+    ft = "ruby",
+    config = function()
+      vim.g.rubycomplete_buffer_loading = 1
+      vim.g.rubycomplete_classes_in_global = 1
+      vim.g.rubycomplete_rails = 1
+    end,
+  },
+
+  {
+    "tpope/vim-endwise",
+    ft = "ruby",
+  },
+
+  {
     "tpope/vim-fugitive",
     cmd = "Git"
   },
