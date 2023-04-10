11ef1889b082a5d9566ca8e21cc242d195323673
Andy Huynh <andy4thehuynh@gmail.com>
2023-04-10 16:12:50 -0600

    ALH: Remove DAP settings bc can not figure out how to use it proper

--- config/nvim/custom/remaps.lua
+++ config/nvim/custom/remaps.lua
@@ -7,10 +7,6 @@ M.general = {
     ["\\\\"] = { ":vsplit<cr>", "splits vim pane vertically" },
     ["<leader>km"] = { ":Telescope keymaps<cr>", "shows current keymaps" },
     ["<leader>g"] = { vim.cmd.Git, "opens fugitive git" },
-    -- ["<leader>db"] = { ":lua require'dap'.toggle_breakpoint()<cr>", "toggles debugger breakpoint" },
-    -- ["<leader>dc"] = { ":lua require'dap'.continue()<cr>", "continue current debugger" },
-    -- ["<leader>ds"] = { ":lua require'dap'.step_over()<cr>", "step over current debugger" },
-    -- ["<leader>do"] = { ":lua require'dap'.repl.open()<cr>", "inspect debugger state via REPL" },
   },
 }
 
