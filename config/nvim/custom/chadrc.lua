4b4680cd3bbd12bdd5dacb34344226bd28946444
Andy Huynh <andy4thehuynh@gmail.com>
2023-04-29 14:01:30 -0600

    ALH: Change status line and and cmp look

--- config/nvim/custom/chadrc.lua
+++ config/nvim/custom/chadrc.lua
@@ -1,9 +1,16 @@
 local M = {}
 
 M.ui = {
-  theme = 'catppuccin',
+  theme = 'chadracula',
+  statusline = {
+    theme = "vscode_colored", -- default/vscode/vscode_colored/minimal
+  },
+  cmp = {
+    style = 'atom_colored', -- default/flat_light/flat_dark/atom/atom_colored
+  },
 }
 
 M.plugins = 'custom.plugins'
 M.mappings = require "custom.remaps"
+
 return M
