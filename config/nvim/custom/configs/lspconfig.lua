7dc830c02549a14966bb584d2ab7d1f938e88efa
Andy Huynh <andy4thehuynh@gmail.com>
2023-04-29 14:48:38 -0600

    ALH: Refactor lsp overrides and include ruby-lsp from Shopify

--- config/nvim/custom/configs/lspconfig.lua
+++ config/nvim/custom/configs/lspconfig.lua
@@ -1,6 +1,5 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
-
 local lspconfig = require("lspconfig")
 
 local servers = {
@@ -11,19 +10,54 @@ local servers = {
   "tsserver",
 }
 
-for _, lsp in ipairs(servers) do
-  -- sets up tailwind colors
-  if lsp == "tailwindcss" then
 vim.diagnostic.config { virtual_text = false }
+
+local function merge(t1, t2)
+  if not t2 then return t1 end
+  return vim.tbl_extend('force', t1, t2)
+end
+
+local overrides = {
+  ruby_ls = {
+    on_attach = function(client, bufnr)
+
+      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePre', 'CursorHold' }, {
+        buffer = bufnr,
+
+        callback = function()
+          local params = vim.lsp.util.make_text_document_params(bufnr)
+
+          client.request(
+            'textDocument/diagnostic',
+            { textDocument = params },
+            function(err, result)
+              if err then return end
+              if not result then return end
+
+              vim.lsp.diagnostic.on_publish_diagnostics(
+                nil,
+                vim.tbl_extend('keep', params, { diagnostics = result.items }),
+                { client_id = client.id }
+              )
+            end
+          )
+        end,
+      })
+    end,
+  },
+
+  tailwindcss = {
     on_attach = function(_, bufnr)
       require("tailwindcss-colors").buf_attach(bufnr)
     end
-  end
+  },
+}
 
-  lspconfig[lsp].setup({
-    on_attach = on_attach,
-    capabilities = capabilities
-  })
+local defaults = {
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup(merge(defaults, overrides[lsp]))
 end
-
-vim.lsp.set_log_level 'debug'
