f6e24c6fa4c4fb885169c17f78b7982a43208421
Andy Huynh <andy4thehuynh@gmail.com>
2024-01-30 15:20:41 -0700

    Add neotree for file browsing with key mapping

--- config/nvim/lua/custom/plugins/init.lua
+++ config/nvim/lua/custom/plugins/init.lua
@@ -11,6 +11,21 @@ M.plugins = {
     -- Will not load on startup, but only when used
     event = 'VeryLazy',
   },
+
+  -- File Browsing
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    config = {
+      vim.keymap.set('n', '<C-P>', ":Neotree toggle<CR>", { desc = 'Toggle File Browser' })
+    }
+  },
 }
 
 return M.plugins
