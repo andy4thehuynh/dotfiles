f9539bcd5247b5e30fd60a7eedeae8b73cb82e0a
Andy Huynh <andy4thehuynh@gmail.com>
2024-06-16 16:01:39 -0600

    Remove Fish, Kitty, Neovim (nvim) and ASDF configs

--- setup.sh
+++ setup.sh
@@ -2,10 +2,12 @@
 
 DOTFILES_DIR="$HOME/code/dotfiles"
 
-ln -vsfn "$DOTFILES_DIR/config/fish" "$HOME/.config/fish"
-ln -vsfn "$DOTFILES_DIR/config/nvim" "$HOME/.config/nvim"
 
+# Link individual files
 ln -vsfn "$DOTFILES_DIR/git/_gitconfig" "$HOME/.gitconfig"
 ln -vsfn "$DOTFILES_DIR/git/_gitignore_global" "$HOME/.gitignore_global"
-ln -vsfn "$DOTFILES_DIR/config/asdf/_tool-versions" "$HOME/.tool-versions"
-ln -vsfn "$DOTFILES_DIR/config/starship.toml" "$HOME/.config/starship.toml"
+ln -vsfn "$DOTFILES_DIR/config/zsh/_zshrc" "$HOME/.zshrc"
+# ln -vsfn "$DOTFILES_DIR/config/starship.toml" "$HOME/.config/starship.toml"
+
+# Link folders
+# ln -vsfn "$DOTFILES_DIR/config/nvim" "$HOME/.config/nvim"
