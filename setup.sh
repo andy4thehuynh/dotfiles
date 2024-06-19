c28a91181beba7124daacc2cd20a070a3d2d28fb
Andy Huynh <andy4thehuynh@gmail.com>
2024-06-19 10:08:59 -0600

    Add mise global config for runtime mgmt

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ DOTFILES_DIR="$HOME/code/dotfiles"
 ln -vsfn "$DOTFILES_DIR/git/_gitconfig" "$HOME/.gitconfig"
 ln -vsfn "$DOTFILES_DIR/git/_gitignore_global" "$HOME/.gitignore_global"
 ln -vsfn "$DOTFILES_DIR/config/zsh/_zshrc" "$HOME/.zshrc"
+ln -vsfn "$DOTFILES_DIR/config/mise/config.toml" "$HOME/.config/mise/config.toml"
 # ln -vsfn "$DOTFILES_DIR/config/starship.toml" "$HOME/.config/starship.toml"
 
 # Link folders
