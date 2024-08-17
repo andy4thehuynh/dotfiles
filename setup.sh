85486f38db3de2a73d0be2f160cc31eedd84ece8
Andy Huynh <andy4thehuynh@gmail.com>
2024-08-17 02:24:13 -0600

    Update link commands for Alacritty, nvim and zsh

--- setup.sh
+++ setup.sh
@@ -2,13 +2,12 @@
 
 DOTFILES_DIR="$HOME/code/dotfiles"
 
-
-# Link individual files
+# Links files
 ln -vsfn "$DOTFILES_DIR/git/_gitconfig" "$HOME/.gitconfig"
 ln -vsfn "$DOTFILES_DIR/git/_gitignore_global" "$HOME/.gitignore_global"
-ln -vsfn "$DOTFILES_DIR/config/zsh/_zshrc" "$HOME/.zshrc"
 ln -vsfn "$DOTFILES_DIR/config/mise/config.toml" "$HOME/.config/mise/config.toml"
-# ln -vsfn "$DOTFILES_DIR/config/starship.toml" "$HOME/.config/starship.toml"
+ln -vsfn "$DOTFILES_DIR/config/zsh/_zshrc" "$HOME/.zshrc"
 
-# Link folders
-# ln -vsfn "$DOTFILES_DIR/config/nvim" "$HOME/.config/nvim"
+# Links folders
+ln -vsfn "$DOTFILES_DIR/config/nvim" "$HOME/.config/nvim"
+ln -vsfn "$DOTFILES_DIR/config/alacritty" "$HOME/.config/alacritty"
