6be0d77e1b64550dc2f890c79d375fa1a99692f0
Andy Huynh <andy4thehuynh@gmail.com>
2024-08-17 19:56:31 -0600

    Add Docker for Raycast

---
+++ config/raycast/extensions/6d35b3a5-ae12-48b1-b5de-616a22387eae/container_list.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:06a6937dfcb838cbbe8f3214530a82237d779306aede3922140bdc5d6bbea621
+size 147606
---
+++ config/raycast/extensions/6d35b3a5-ae12-48b1-b5de-616a22387eae/projects_list.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1d2533b688a7340f8d2ac47f58116022312ce7618d39294e216c9aca023f6edd
+size 148804
