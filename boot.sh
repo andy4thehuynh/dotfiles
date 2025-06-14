846bcd6e3835580d30bed6ff89b83a0219547869
Andy Huynh <andy4thehuynh@gmail.com>
2025-06-14 10:45:33 -0600

    Change dotfiles dir variable

--- boot.sh
+++ boot.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-DOTFILES_DIR="$HOME/Code/dotfiles"
+DOT_DIR="$HOME/Code/dotfiles"
 
 # Links files
-ln -vsfn "$DOTFILES_DIR/git/_gitconfig" "$HOME/.gitconfig"
-ln -vsfn "$DOTFILES_DIR/git/_gitignore_global" "$HOME/.gitignore_global"
-ln -vsfn "$DOTFILES_DIR/config/mise/config.toml" "$HOME/.config/mise/config.toml"
-ln -vsfn "$DOTFILES_DIR/config/zsh/_zshrc" "$HOME/.zshrc"
+ln -vsfn "$DOT_DIR/git/_gitconfig" "$HOME/.gitconfig"
+ln -vsfn "$DOT_DIR/git/_gitignore_global" "$HOME/.gitignore_global"
+ln -vsfn "$DOT_DIR/config/mise/config.toml" "$HOME/.config/mise/config.toml"
+ln -vsfn "$DOT_DIR/config/zsh/_zshrc" "$HOME/.zshrc"
 
 # Links folders
-ln -vsfn "$DOTFILES_DIR/config/alacritty" "$HOME/.config/alacritty"
-ln -vsfn "$DOTFILES_DIR/config/nvim.bak" "$HOME/.config/nvim.bak"
+ln -vsfn "$DOT_DIR/config/nvim" "$HOME/.config/nvim"
+ln -vsfn "$DOT_DIR/config/nvim.bak" "$HOME/.config/nvim.bak"
