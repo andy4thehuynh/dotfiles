6c14f1b0d58e7991137c2dadfc7cb3edcdf7a83b
Andy Huynh <andy4thehuynh@gmail.com>
2025-06-20 04:21:08 -0600

    Remove nvim backup

--- config/nvim.bak/lua/config/options.lua
+++
@@ -1,8 +0,0 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-
--- Disables swapfiles
-vim.api.nvim_set_option("swapfile", false)
-vim.api.nvim_set_option("backup", false)
-vim.api.nvim_set_option("writebackup", false)
