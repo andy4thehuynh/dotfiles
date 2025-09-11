6758c757bdd76e8db6cc05907ca87c4f0ab3c855
Andy Huynh <2831414+andy4thehuynh@users.noreply.github.com>
2025-09-11 08:19:18 -0600

    Add basic neovim config for editing files

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,3 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.relativenumber = true
+vim.opt.ignorecase = true -- ignore case in search patterns
+vim.opt.smartcase = true -- override ignorecase if search contains uppercase
+vim.opt.swapfile = false
