71c57a4ba794ad8f68daa03413f80fbdabb2c144
Andy Huynh <2831414+andy4thehuynh@users.noreply.github.com>
2025-10-11 06:50:49 -0600

    Update nvim packages and add sensible nvim defaults

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,7 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.opt.number = true -- show line numbers
 vim.opt.relativenumber = true
 vim.opt.ignorecase = true -- ignore case in search patterns
 vim.opt.smartcase = true -- override ignorecase if search contains uppercase
-vim.opt.swapfile = false
+vim.opt.swapfile = false -- swapfiles are overkill for your workflow
+vim.opt.clipboard = "unnamedplus" -- use system clipboard
