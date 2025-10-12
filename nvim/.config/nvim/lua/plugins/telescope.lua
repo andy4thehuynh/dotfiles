9ff6c20c99bcbf8fd2e5e42703ef68e5fbbf5ec2
Andy Huynh <2831414+andy4thehuynh@users.noreply.github.com>
2025-10-11 22:20:30 -0600

    Shows hidden files for dotfiles for fzf

---
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,36 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  opts = {
+    defaults = {
+      file_ignore_patterns = {
+        "^.git/",
+        "node_modules/",
+        "%.pyc$",
+        "__pycache__/",
+        "%.egg%-info/",
+        "dist/",
+        "build/",
+        "vendor/",
+        "%.gem$",
+        "%.so$",
+        "%.dylib$",
+      },
+    },
+  },
+  keys = {
+    {
+      "<leader><space>",
+      function()
+        require("telescope.builtin").find_files({ hidden = true })
+      end,
+      desc = "Find Files (Root Dir)",
+    },
+    {
+      "<leader>ff",
+      function()
+        require("telescope.builtin").find_files({ hidden = true, cwd = vim.uv.cwd() })
+      end,
+      desc = "Find Files (cwd)",
+    },
+  },
+}
