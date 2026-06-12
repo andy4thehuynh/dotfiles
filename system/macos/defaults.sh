3421a9f0679c898cd670ea5e9a6fb12ed9a17b7d
Andy Huynh <2831414+andy4thehuynh@users.noreply.github.com>
2026-06-11 18:27:47 -0600

    Add dock autohide and left-side orientation to macOS defaults

--- system/macos/defaults.sh
+++ system/macos/defaults.sh
@@ -2,4 +2,11 @@
 
 # Remove recently opened applications from dock
 defaults write com.apple.dock show-recents -bool false
+
+# Hide the dock automatically
+defaults write com.apple.dock autohide -bool true
+
+# Position dock on the left
+defaults write com.apple.dock orientation -string left
+
 killall Dock
